f79681d86667b117412cd2ffbb190650f9a87f6d
AlSolor <alansolora@gmail.com>
2019-09-11 18:16:46 -0500

    Tercera sincronizacion

--- archivo.java
+++ archivo.java
@@ -1,9 +1,12 @@
 public class archivo{
+    public static void main(String[] args){
 	System.out.println("nombre");
 	int x,i;
 	x=0;
-	for(i=0;i<10;i++){
+	for(i=0;i<10;){
 		System.out.println(x);
 		x++;
+		i++;
 	}
+    }
 }
