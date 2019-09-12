a4109f52dc185a0b987e7af764fc3d34fab373ae
Braulio-Tov <bayotron@gmail.com>
2019-09-11 19:21:30 -0500

    primer sincronizacion

--- archivo.java
+++ archivo.java
@@ -1,12 +1,13 @@
 public class archivo{
     public static void main(String[] args){
-	System.out.println("nombre");
-	int x,i;
-	x=0;
-	for(i=0;i<10;){
-		System.out.println(x);
-		x++;
-		i++;
-	}
+		System.out.println("nombre");
+		int x,i;
+		x=0;
+		for(i=0;i<10;){
+			System.out.println(x);
+			x++;
+			i++;
+		}
+		System.out.println("HOLA")
     }
 }
